2df4edf9e498aa92de06977f3ed47a6683daea37
randi.apriansyah <abityura@gmail.com>
2018-07-02 15:49:16 +0700

    Add User Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const gratavar = require("gravatar");
 
-router.get("/test", (req,res) => res.json({msg : "Test API users OK!!!"}));
+//load User model
+const User = require("../../models/User");
+
+// @route   GET   api/users/register
+// @desc    Go to Registration Page
+// @access  Public
+router.get("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const avatar = gratavar.url(req.body.email, {
+        s: "200", // size
+        r: "pg", // rating
+        d: "mm" // Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
-
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+//bodyparser middleware config with express
+app.use(bodyParser.urlEncoded({ extended: false }));
+app.use(bodyParser.json());
 
 //import mongo key
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 const port = process.env.port || 5000;
 
@@ -11,16 +16,14 @@ const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
-
-
 //connect to mongodb
 mongoose
   .connect(db)
   .then(() => console.log("Server connected to mongodb"))
   .catch(err => console.log(err));
 
-app.get("/", (req,res) => res.send("Server running....."));
+app.get("/", (req, res) => res.send("Server running....."));
 app.use("/api/users", users);
 app.use("/api/posts", posts);
-app.use("/api/profile" , profile);
+app.use("/api/profile", profile);
 app.listen(port, () => console.log(`Server running on port ${port}`));
